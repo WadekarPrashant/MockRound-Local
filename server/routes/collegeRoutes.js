26e3ad45a891580e724c814af99e4f197305c7a8
sharmatatsat23 <sharmatatsat23@gmail.com>
2024-07-10 11:23:44 +0530

    authentication using jwt

--- server/routes/collegeRoutes.js
+++ server/routes/collegeRoutes.js
@@ -1,8 +1,39 @@
 const express = require('express');
-const { addCollege, getColleges } = require('../controllers/collegeController');
 const router = express.Router();
+const College = require('../models/College');
 
-router.post('/add', addCollege);
-router.get('/', getColleges);
+// Route to create a new college entry
+router.post('/add', async (req, res) => {
+    const { collegeName, address, coursesAvailable, cutOffSpotRound, casteCategoryCutOff, minStudentCriteria, maxCriteria, spotRoundDates, approvedBy } = req.body;
+
+    try {
+        const newCollege = new College({
+            collegeName,
+            address,
+            coursesAvailable,
+            cutOffSpotRound,
+            casteCategoryCutOff,
+            minStudentCriteria,
+            maxCriteria,
+            spotRoundDates,
+            approvedBy
+        });
+
+        await newCollege.save();
+        res.status(201).json(newCollege);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+});
+
+// Route to get all college entries
+router.get('/', async (req, res) => {
+    try {
+        const colleges = await College.find();
+        res.json(colleges);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
 
 module.exports = router;
