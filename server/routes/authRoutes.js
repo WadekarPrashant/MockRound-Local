2ea993f7a59e7abf42775f68ee24dad833a58708
sharmatatsat23 <sharmatatsat23@gmail.com>
2024-07-12 16:17:00 +0530

    routes updated

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -2,15 +2,15 @@ require('dotenv').config();
 const express = require('express');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 const User = require('../models/User');
-const JWT_SECRET = process.env.JWT_SECRET;
+const JWT_SECRET = process.env.JWT_SECRET || 'secretkey';
 
 const generateToken = (id) => {
-    return jwt.sign({ id }, JWT_SECRET || 'secretkey', {
+    return jwt.sign({ id }, JWT_SECRET, {
         expiresIn: '30d',
     });
 };
-
 
 router.post('/signup', async (req, res) => {
     const { email, password } = req.body;
@@ -20,11 +20,11 @@ router.post('/signup', async (req, res) => {
             return res.status(400).json({ error: 'Email and password are required' });
         }
 
+        const hashedPassword = await bcrypt.hash(password, 10); // Hash the password
 
-        const newUser = new User({ email, password });
+        const newUser = new User({ email, password: hashedPassword });
         await newUser.save();
 
-        
         const token = generateToken(newUser._id);
 
         res.status(201).json({
@@ -32,6 +32,34 @@ router.post('/signup', async (req, res) => {
             email: newUser.email,
             token: token,
         });
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+});
+
+router.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        if (!email || !password) {
+            return res.status(400).json({ error: 'Email and password are required' });
+        }
+
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return res.status(401).json({ error: 'Invalid email or password' });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+            return res.status(401).json({ error: 'Invalid email or password' });
+        }
+
+        const token = generateToken(user._id);
+
+        res.json({ _id: user._id, email: user.email, token });
     } catch (error) {
         res.status(400).json({ error: error.message });
     }
